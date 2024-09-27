3291c3f93df09f2dfd268220fe4fa49cdf04dbb3
tanmayiscool <tanmayrajput2905@gmail.com>
2024-09-27 14:41:15 +0530

    first commit

--- program1.py
+++ program1.py
@@ -1,20 +1,22 @@
 class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-
-
-
-
-
-
-
+   def is_valid_parentheses(s: str) -> bool:
+    stack = [s]
+    bracket_map = {')': '(', '}': '{', ']': '['}
     
+    for char in s:
+        if char in bracket_map.values():
+            stack.append(char) 
+        elif char in bracket_map.keys():
+            if not stack or stack[-1] != bracket_map[char]:
+                return False  
+            stack.pop() 
+            
+    return not stack  
 
 
-
-  
-
+user_input = input("Enter a string of parentheses: ")
+x=Solution.is_valid_parentheses(user_input)
+if x:
+    print("true")
+else:
+    print("false")
